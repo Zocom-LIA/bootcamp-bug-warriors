46d8d8c34c244820d110d08c1eb66ada027e88be
P156HAM <a.hamdi.almasalmeh@gmail.com>
2023-12-21 20:30:42 +0100

    feat: add-cart-button-second-push

--- frontend/packages/pages/cart/ui/cart-button/ui/index.tsx
+++ frontend/packages/pages/cart/ui/cart-button/ui/index.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CartTop, CartBottom } from '@zocom/cart-button';
+import { useSelector } from 'react-redux';
 import './style.scss'
+import { RootState } from '@zocom/store';
 
 export enum CartButtonStyles {
   'MENU' = 'menu',
   'CART' = 'cart'
 }
 
+export enum Animation {
+  'ANIMATE' = 'cart-handle-animate',
+  'NONE' = ''
+}
 export type CartButtonProps = {
   style?: CartButtonStyles
+  animate?: Animation | string;
 }
 
 
-export const CartButton = ({ style }: CartButtonProps) => {
+export const CartButton = ({ style, animate }: CartButtonProps) => {
+  const cartCount = useSelector((state: RootState) => state.cart?.items?.length) 
+
   return (
-    <div className={`button-${style}`}>
-      <span>1</span>
+    <div className={`cart-button__${style} `}>
+      <span className='cart-count'>{cartCount}</span>
       <span className='cart-logo'>
-        <img src={CartTop} alt='cart logo'/>
-        <img src={CartBottom} alt='cart logo'/>
+        <img className={`cart-logo__top ${animate || ''}`} src={CartTop} alt='cart logo'/>
+        <img className='cart-logo__bottom' src={CartBottom} alt='cart logo'/>
       </span>
     </div>
   )
