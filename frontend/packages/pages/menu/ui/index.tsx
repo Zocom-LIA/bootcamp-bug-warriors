23368c2269a4359c6fe605c9f9ce9738c69d6584
Tibalomium <33953814+Tibalomium@users.noreply.github.com>
2024-01-02 14:35:15 +0100

    feat: menu item
    Items for displaying menu

--- frontend/packages/pages/menu/ui/index.tsx
+++ frontend/packages/pages/menu/ui/index.tsx
@@ -1,95 +1,277 @@
 import "./style.scss";
 import { MenuContainer } from "@zocom/menu-container";
 import { Styles, Wrapper } from "@zocom/wrapper";
-import { addItem, decrease, increase } from '@zocom/cart-actions'
-import { RootState } from '@zocom/store'
-import { useDispatch, useSelector } from 'react-redux'
-import { Product } from '@zocom/types'
+import { addItem, decrease, increase } from "@zocom/cart-actions";
+import { RootState } from "@zocom/store";
+import { useDispatch, useSelector } from "react-redux";
+import { Product } from "@zocom/types";
 import { Button } from "@zocom/button";
-import { CartButton, CartButtonStyles, Animation } from "@zocom/cart-button"
+import { CartButton, CartButtonStyles, Animation } from "@zocom/cart-button";
+import { MenuButton } from "@zocom/menu-button";
 import { useEffect, useState } from "react";
 
+export const Menu = () => {
+  const [animate, setAnimate] = useState(false);
+  const cartItems = useSelector((state: RootState) => state.cart?.items);
+  console.log(cartItems);
+  const dispatch = useDispatch();
 
-export const  Menu = () => {
-    const [animate, setAnimate] = useState(false);
-    const cartItems = useSelector((state: RootState) => state.cart?.items)   
-    console.log(cartItems)
-    const dispatch = useDispatch()
+  const handleAddItem = (item: Product) => {
+    dispatch(addItem(item));
+    setAnimate(true);
+  };
+  const handleIncrease = (item: Product) => {
+    dispatch(increase(item));
+  };
+  const handleDecrease = (item: Product) => {
+    dispatch(decrease(item));
+  };
 
-    const handleAddItem = (item: Product) => {
-        dispatch(addItem(item))
-        setAnimate(true);
+  useEffect(() => {
+    if (animate) {
+      // Remove the animation class after it completes
+      const timer = setTimeout(() => {
+        setAnimate(false);
+      }, 600); // This should match the duration of your animation
+      return () => clearTimeout(timer);
     }
-    const handleIncrease = (item: Product) => {
-        dispatch(increase(item))
-    }
-    const handleDecrease = (item: Product) => {
-        dispatch(decrease(item))
-    }
+  }, [animate]);
 
-    useEffect(() => {
-        if (animate) {
-          // Remove the animation class after it completes
-          const timer = setTimeout(() => {
-            setAnimate(false);
-          }, 600); // This should match the duration of your animation
-          return () => clearTimeout(timer);
-        }
-      }, [animate]);
-
-    return(
-
-        <Wrapper style={Styles.MAIN} >
-            <MenuContainer>
-                <CartButton style={CartButtonStyles.MENU} animate={animate ? Animation.ANIMATE : Animation.NONE}></CartButton>
-                <h1 className="quote">Karlstad</h1>
-                <Button onClick={() => handleAddItem( {"name":"Karlstad","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>Add item</Button>
-                <Button onClick={() => handleIncrease({"name":"Karlstad","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>+</Button>
-                <Button onClick={() => handleDecrease({"name":"Karlstad","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>-</Button>
-                <h1 className="quote">Bangkok</h1>
-                <Button onClick={() => handleAddItem( {"name":"Bangkok","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>Add item</Button>
-                <Button onClick={() => handleIncrease({"name":"Bangkok","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>+</Button>
-                <Button onClick={() => handleDecrease({"name":"Bangkok","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>-</Button>
-                <h1 className="quote">Paris</h1>
-                <Button onClick={() => handleAddItem( {"name":"Paris","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>Add item</Button>
-                <Button onClick={() => handleIncrease({"name":"Paris","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>+</Button>
-                <Button onClick={() => handleDecrease({"name":"Paris","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>-</Button>
-                <h1 className="quote">Oaxaca</h1>
-                <Button onClick={() => handleAddItem( {"name":"Oaxaca","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>Add item</Button>
-                <Button onClick={() => handleIncrease({"name":"Oaxaca","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>+</Button>
-                <Button onClick={() => handleDecrease({"name":"Oaxaca","desc":"En god friterad wonton.","ingredients":["kantarell","scharlottenlök","morot","bladpersilja"],"price":9})}>-</Button>
-            </MenuContainer>
-        </Wrapper>
-
-    )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return (
+    <Wrapper style={Styles.MAIN}>
+      <MenuContainer>
+        <CartButton
+          style={CartButtonStyles.MENU}
+          animate={animate ? Animation.ANIMATE : Animation.NONE}
+        ></CartButton>
+        <h1 className="quote">Karlstad</h1>
+        <MenuButton
+          menuItem={["WONTON", "9 sek", "persilja"]}
+          onClick={() =>
+            handleAddItem({
+              name: "Karlstad",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        ></MenuButton>
+        <Button
+          onClick={() =>
+            handleAddItem({
+              name: "Karlstad",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          Add item
+        </Button>
+        <Button
+          onClick={() =>
+            handleIncrease({
+              name: "Karlstad",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          +
+        </Button>
+        <Button
+          onClick={() =>
+            handleDecrease({
+              name: "Karlstad",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          -
+        </Button>
+        <h1 className="quote">Bangkok</h1>
+        <Button
+          onClick={() =>
+            handleAddItem({
+              name: "Bangkok",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          Add item
+        </Button>
+        <Button
+          onClick={() =>
+            handleIncrease({
+              name: "Bangkok",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          +
+        </Button>
+        <Button
+          onClick={() =>
+            handleDecrease({
+              name: "Bangkok",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          -
+        </Button>
+        <h1 className="quote">Paris</h1>
+        <Button
+          onClick={() =>
+            handleAddItem({
+              name: "Paris",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          Add item
+        </Button>
+        <Button
+          onClick={() =>
+            handleIncrease({
+              name: "Paris",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          +
+        </Button>
+        <Button
+          onClick={() =>
+            handleDecrease({
+              name: "Paris",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          -
+        </Button>
+        <h1 className="quote">Oaxaca</h1>
+        <Button
+          onClick={() =>
+            handleAddItem({
+              name: "Oaxaca",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          Add item
+        </Button>
+        <Button
+          onClick={() =>
+            handleIncrease({
+              name: "Oaxaca",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          +
+        </Button>
+        <Button
+          onClick={() =>
+            handleDecrease({
+              name: "Oaxaca",
+              desc: "En god friterad wonton.",
+              ingredients: [
+                "kantarell",
+                "scharlottenlök",
+                "morot",
+                "bladpersilja",
+              ],
+              price: 9,
+            })
+          }
+        >
+          -
+        </Button>
+      </MenuContainer>
+    </Wrapper>
+  );
+};
 
 // import "./style.scss";
 
