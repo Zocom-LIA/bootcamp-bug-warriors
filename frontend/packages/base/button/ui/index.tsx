dc1f01b41cfd7dbeb1e065467b4810cf193c6fad
Tibalomium <33953814+Tibalomium@users.noreply.github.com>
2024-01-04 09:54:42 +0100

    feat: Custom button
    Custom button

--- frontend/packages/base/button/ui/index.tsx
+++ frontend/packages/base/button/ui/index.tsx
@@ -1,29 +1,36 @@
-import './style.scss';
-import { ReactNode } from 'react';
-import { StyleTypes } from '@zocom/types';
+import "./style.scss";
+import { ReactNode } from "react";
+import { StyleTypes } from "@zocom/types";
+import React from "react";
 
 /* Local Component Types */
 export enum ButtonType {
-    'REGULAR' = 'regular',
-    'STRETCH' = 'stretch',
-    'MENU' = 'menu'
+  "REGULAR" = "regular",
+  "STRETCH" = "stretch",
+  "MENU" = "menu",
+  "INVERTED" = "inverted",
+  "READY" = "ready",
+  "SERVED" = "served",
 }
 
 /* Component Props */
 type ButtonProps = {
-    children: ReactNode | ReactNode[];
-    style?: StyleTypes;
-    type?: ButtonType; 
-    onClick: () => void;
-}
+  children: ReactNode | ReactNode[];
+  style?: StyleTypes;
+  type?: ButtonType;
+  onClick: () => void;
+};
 
 /* Component */
-export const Button = ({ 
-    children, 
-    type = ButtonType.REGULAR, // default value
-    style = StyleTypes.DEFAULT, // default value
-    onClick
+export const Button = ({
+  children,
+  type = ButtonType.REGULAR, // default value
+  style = StyleTypes.DEFAULT, // default value
+  onClick,
 }: ButtonProps) => {
-    return (
-        <button className={`button__${type}--${style}`} onClick={() => onClick()}>{children}</button>)
-}
+  return (
+    <button className={`button__${type}--${style}`} onClick={() => onClick()}>
+      {children}
+    </button>
+  );
+};
