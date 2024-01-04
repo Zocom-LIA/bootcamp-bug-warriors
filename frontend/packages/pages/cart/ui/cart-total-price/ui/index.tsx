7f45049187e18e866701667576e6d99087021c5e
Tibalomium <33953814+Tibalomium@users.noreply.github.com>
2024-01-04 12:50:01 +0100

    feat: Total Price in Cart
    Displaying the total price in cart

--- frontend/packages/pages/cart/ui/cart-total-price/ui/index.tsx
+++ frontend/packages/pages/cart/ui/cart-total-price/ui/index.tsx
@@ -1,22 +1,29 @@
 import "./style.scss";
 import { ReactNode } from "react";
-import { StyleTypes } from "@zocom/types";
 import React from "react";
+import { Product } from "@zocom/types";
 
-/* Component Props */
 type CartTotalPriceProps = {
-  price: ReactNode;
+  wonton: Product[];
+  dip: Product[];
 };
 
 /* Component */
-export const CartTotalPrice = ({ price }: CartTotalPriceProps) => {
+export const CartTotalPrice = ({
+  wonton = [],
+  dip = [],
+}: CartTotalPriceProps) => {
+  const totalPrice = [...wonton, ...dip].reduce((tPrice, item) => {
+    return tPrice + item.price * item.quantity;
+  }, 0);
+
   return (
     <div className={"cart-total-price"}>
       <div>
         <div className="cart-text-size-medium">TOTALT</div>
         <div className="cart-text-size-small">inkl 20% moms</div>
       </div>
-      <span className="cart-text-size-large">{price} SEK</span>
+      <span className="cart-text-size-large">{totalPrice} SEK</span>
     </div>
   );
 };
