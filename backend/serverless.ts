c8de357e346b8ac8e680b6f17772c4f182f91a94
Hamdi Al Masalmeh <112500994+P156HAM@users.noreply.github.com>
2024-01-05 08:48:05 +0100

    chore: bug-fix-database

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -1,13 +1,13 @@
-import type { AWS } from '@serverless/typescript';
-import providerConfig from 'src/serverless/seedData/provider';
-import { functions } from 'src/serverless/seedData/functions';
-import { customConfig } from 'src/serverless/seedData/esbuild';
-import { DynamoResources } from 'src/serverless/seedData/dynamodb';
+import type { AWS } from "@serverless/typescript";
+import providerConfig from "src/serverless/seedData/provider";
+import { functions } from "src/serverless/seedData/functions";
+import { customConfig } from "src/serverless/seedData/esbuild";
+import { DynamoResources } from "src/serverless/seedData/dynamodb";
 
 const serverlessConfiguration: AWS = {
-  service: 'backend',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "YUM-YUM",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: providerConfig,
   functions: functions,
   custom: customConfig,
--- backend/src/serverless/seedData/provider.ts
+++ backend/src/serverless/seedData/provider.ts
@@ -1,12 +1,9 @@
 const providerConfig = {
-  name: 'aws',
-  runtime: 'nodejs18.x',
-  profile: process.env.AWS_PROFILE,
-  region: 'eu-north-1',
-  iam: {
-    role: process.env.AWS_IAM_ROLE,
-  },
-  deploymentMethod: 'direct',
+  name: "aws",
+  runtime: "nodejs18.x",
+  profile: "Main",
+  region: "eu-north-1",
+  deploymentMethod: "direct",
   environment: {
     YUM_YUM_TABLE: "Yum-Yum-table",
   },
