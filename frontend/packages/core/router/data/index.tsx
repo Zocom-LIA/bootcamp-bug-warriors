0c5e54460e6e5aa1639fbe103b20d798e678b02e
Marvan Al Kufai <alkufai@gmail.com>
2024-01-08 15:28:22 +0100

    chore: finished container design

--- frontend/packages/core/router/data/index.tsx
+++ frontend/packages/core/router/data/index.tsx
@@ -1,41 +1,38 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-  
-  import { Menu } from "@zocom/menu";
-  import { Cart } from "@zocom/cart";
-  // import { DashboardPage } from "@zocom/dashboard-page";
-  // import { OrderStatusPage } from "@zocom/orderStatus-page";
-  // import { ReciptPage } from "@zocom/recipt-page";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Menu />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
-    },
-    // {
-    //   path: "/status",
-    //   element: <OrderStatusPage />,
-    // },
-    // {
-    //   path: "/dashboard",
-    //   element: <DashboardPage />,
-    // },
-    // {
-    //   path: "/recipt",
-    //   element: <ReciptPage />,
-    // },
-    {
-      path: "*",
-      element: <p>Page Not Found</p>
-    },
-  ]);
-  
-  export const AppRoutes = () => {
-    return <RouterProvider router={router} />;
-  };
+import { Menu } from '@zocom/menu';
+import { Cart } from '@zocom/cart';
+// import { DashboardPage } from "@zocom/dashboard-page";
+import { OrderStatusPage } from '@zocom/order-status-page';
+// import { ReciptPage } from "@zocom/recipt-page";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/status',
+    element: <OrderStatusPage />,
+  },
+  // {
+  //   path: "/dashboard",
+  //   element: <DashboardPage />,
+  // },
+  // {
+  //   path: "/recipt",
+  //   element: <ReciptPage />,
+  // },
+  {
+    path: '*',
+    element: <p>Page Not Found</p>,
+  },
+]);
+
+export const AppRoutes = () => {
+  return <RouterProvider router={router} />;
+};
--- frontend/packages/pages/order-status/ui/index.tsx
+++ frontend/packages/pages/order-status/ui/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './style.scss';
+import { img } from '@zocom/status-img';
+import { Status } from '@zocom/status';
+import { TopBar } from '@zocom/top-bar';
+import { Styles, Wrapper } from '@zocom/wrapper';
+import { ButtonType, Button } from '@zocom/button';
 
-export const OrderStatus = () => {
+export const OrderStatusPage = () => {
   return (
-    <div>
-      <h1>Order Status</h1>
-    </div>
+    <Wrapper style={Styles.MAIN}>
+      <TopBar />
+      <img src={img} />
+      <Status eta={4} orderNr='232edd2' />
+      <div className='status__button-container'>
+        <Button type={ButtonType.REGULAR} onClick={() => console.log('mer')}>
+          BESTÄLL MER
+        </Button>
+        <Button
+          type={ButtonType.INVERTED}
+          onClick={() => console.log('kvitto')}
+        >
+          SE KVITTO
+        </Button>
+      </div>
+    </Wrapper>
   );
 };
