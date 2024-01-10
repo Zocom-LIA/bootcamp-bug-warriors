189cb3c8d91328fbe9a67f2a119a13002c5eedd9
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2024-01-10 11:42:24 +0100

    dashboard view fix

--- frontend/packages/pages/dashboard/ui/container__column/ui/index.tsx
+++ frontend/packages/pages/dashboard/ui/container__column/ui/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import "./style.scss";
-import React from "react";
 
 export enum Status {
   Ongoing = "ONGOING",
--- frontend/packages/pages/dashboard/ui/index.tsx
+++ frontend/packages/pages/dashboard/ui/index.tsx
@@ -1,13 +1,17 @@
 import './style.scss';
-import React from 'react';
 import { Styles, Wrapper } from '@zocom/wrapper';
 import { DashboardContainer } from '@zocom/dashboard-container';
 import { DashboardColumn, Status } from '@zocom/dashboard-container__column';
+import { TopBar } from '@zocom/top-bar';
 
 export const DashboardPage = () => {
 
   return (
     <Wrapper style={Styles.TABLET}>
+      <section className='dashboard-top'>
+        <TopBar/>
+        <h1>KITCHEN VIEW</h1>
+      </section>
         <DashboardContainer>
             <DashboardColumn status={Status.Ongoing}>
                 <p>kort</p>
